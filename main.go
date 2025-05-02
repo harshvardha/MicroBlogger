c1ca96a13117b6602c83b5608fa98f9227aab2da
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-05-02 23:34:38 +0530

    added otp cache to apiConfig

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/harshvardha/artOfSoftwareEngineering/controllers"
+	"github.com/harshvardha/artOfSoftwareEngineering/internal/cache"
+	"github.com/harshvardha/artOfSoftwareEngineering/internal/database"
+	"github.com/harshvardha/artOfSoftwareEngineering/middlewares"
+	"github.com/harshvardha/artOfSoftwareEngineering/utility"
 	"github.com/joho/godotenv"
 )
 
@@ -24,9 +31,50 @@ func main() {
 		log.Fatal("port variable not set")
 	}
 
-	// apiConfig := ApiConfig{
-	// 	JwtSecret: jwtSecret,
-	// }
+	// loading db uri
+	dbUri := os.Getenv("DATABASE_URI")
+	if dbUri == "" {
+		log.Fatal("Database URI not set")
+	}
+
+	// loading otp cache required configs
+	fromEmail := os.Getenv("FROM_EMAIL")
+	if fromEmail == "" {
+		log.Fatal("From Email not set")
+	}
+	smtpHost := os.Getenv("SMTP_HOST")
+	if smtpHost == "" {
+		log.Fatal("SMTP HOST not set")
+	}
+	smtpPort := os.Getenv("SMTP_PORT")
+	if smtpPort == "" {
+		log.Fatal("SMTP PORT not set")
+	}
+	appPassword := os.Getenv("GMAIL_APP_PASSWORD")
+	if appPassword == "" {
+		log.Fatal("App Password not set")
+	}
+
+	// creating database connection
+	dbConnection, err := sql.Open("postgres", dbUri)
+	if err != nil {
+		log.Fatal("Error Connecting to Database: ", err)
+	}
+
+	// setting apiConfig
+	apiConfig := controllers.ApiConfig{
+		DB:        database.New(dbConnection),
+		JwtSecret: jwtSecret,
+		OtpCache:  cache.NewOTPCache(fromEmail, smtpHost, smtpPort, appPassword),
+	}
+
+	// setting data validator
+	dataValidator := middlewares.Validator{
+		Validate: validator.New(),
+	}
+
+	// registering new custom password validator
+	dataValidator.Validate.RegisterValidation("password", utility.CustomPasswordValidator)
 
 	// creating new request redirecting multiplexer
 	mux := http.NewServeMux()
@@ -37,6 +85,12 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	// api endpoints for authentication
+	mux.HandleFunc("POST /api/auth/sendOTP", middlewares.AuthValidation(apiConfig.HandleSendOTP, &dataValidator))
+	mux.HandleFunc("POST /api/auth/verifyOTP", apiConfig.HandleRegisterUser)
+	mux.HandleFunc("GET /api/auth/resendOTP", apiConfig.HandleResendOTP)
+	mux.HandleFunc("POST /api/auth/login", middlewares.AuthValidation(apiConfig.HandleLogin, &dataValidator))
 
 	// starting the server
 	server := &http.Server{
