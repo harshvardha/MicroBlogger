4fc360cd6aedbd2ef7129978c674105798f78a3f
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-05-03 20:56:06 +0530

    added DataValidator to ApiCOnfig

--- controllers/controller_types.go
+++ controllers/controller_types.go
@@ -1,15 +1,17 @@
 package controllers
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/harshvardha/artOfSoftwareEngineering/internal/cache"
 	"github.com/harshvardha/artOfSoftwareEngineering/internal/database"
 )
 
 type ApiConfig struct {
-	DB        *database.Queries
-	JwtSecret string
-	OtpCache  *cache.OtpCache
+	DB            *database.Queries
+	JwtSecret     string
+	OtpCache      *cache.OtpCache
+	DataValidator *validator.Validate
 }
 
 type IDAndRole struct {
