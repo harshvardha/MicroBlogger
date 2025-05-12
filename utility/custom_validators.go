0cd4cc6cb019662b96a66ee5945fa40025daf5a4
harshvardhan singh <harshvardhansingh458@protonmail.com>
2025-05-12 10:33:36 +0530

    working properly

--- utility/custom_validators.go
+++ utility/custom_validators.go
@@ -23,3 +23,34 @@ func CustomPasswordValidator(fl validator.FieldLevel) bool {
 
 	return hasUpper && hasLower && hasNumber && hasSpecialCharacter
 }
+
+func S3UrlValidator(fl validator.FieldLevel) bool {
+	return true
+}
+
+func GithubURLValidator(fl validator.FieldLevel) bool {
+	url := fl.Field().String()
+
+	// checking if the url starts with: https://github.com/harshvardha
+	pattern := `^(https?:\/\/)?(www\.)?github\.com\/harshvardha\/[a-zA-Z0-9]+(?:-?[a-zA-Z0-9]+)*(\/)?$`
+	isURLValid := regexp.MustCompile(pattern).MatchString(url)
+	return isURLValid
+}
+
+func TagsValidator(fl validator.FieldLevel) bool {
+	tags := fl.Field().String()
+	pattern := `^[a-zA-z0-9;]`
+	return regexp.MustCompile(pattern).MatchString(tags)
+}
+
+func UsernameValidator(fl validator.FieldLevel) bool {
+	username := fl.Field().String()
+	pattern := `^[a-zA-z0-9_]`
+	return regexp.MustCompile(pattern).MatchString(username)
+}
+
+func BookNameValidator(fl validator.FieldLevel) bool {
+	bookName := fl.Field().String()
+	pattern := `^[a-zA-z0-9_]`
+	return regexp.MustCompile(pattern).MatchString(bookName)
+}
